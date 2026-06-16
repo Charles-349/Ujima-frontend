3da854204554b06f9fea9855d5931e65ede7ba1e
Charles-349 <wamahiucharles123@gmail.com>
2026-06-16 11:57:07 +0300

    baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://ujima-frontend-gold.vercel.app",
   },
 });
